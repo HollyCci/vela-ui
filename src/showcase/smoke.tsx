165d87747929a7533269f915e0953f0a9d6e039e
HollyCc <chencheng.wocc@gmail.com>
2026-06-12 23:14:41 +0800

    chore: prepare v0.1.0 release

--- src/showcase/smoke.tsx
+++ src/showcase/smoke.tsx
@@ -18,7 +18,7 @@ const Smoke = () => (
     </div>
 
     <div className="flex flex-wrap items-center gap-2">
-      <Chip color="primary">Primary</Chip>
+      <Chip color="accent">Accent</Chip>
       <Chip color="success">Success</Chip>
       <Chip color="warning">Warning</Chip>
       <Chip color="danger">Danger</Chip>
